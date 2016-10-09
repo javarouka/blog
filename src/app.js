67dfc36fd2b19be9c95f672e326e303ece1ede68
이항희(JavaRouka) <javarouka@coupang.com>
2016-10-10 00:08:46 +0900

    블로그 작성중

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import './css/index.css'
-import 'prismjs/themes/prism.css'
-import prism from 'prismjs'
+import 'prismjs/themes/prism-okaidia.css'
+import Prism from 'prismjs'
 
 function applySyntaxHighliting() {
     const syntaxTargets = document.querySelectorAll('pre code[data-language]');
     syntaxTargets.forEach(target => {
         const code = target.innerHTML;
-        var html = prism.highlight(code, prism.languages[target.dataset.language] || prism.languages.javascript);
+        var html = Prism.highlight(code, Prism.languages[target.dataset.language] || Prism.languages.javascript);
         target.innerHTML = html;
     });
 }
 
-applySyntaxHighliting();
+Prism.highlightAll();
+//applySyntaxHighliting();
