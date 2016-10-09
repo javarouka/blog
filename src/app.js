5e4f121ffc60c04a1ededdb8a78f8a8c8e8e5282
이항희(JavaRouka) <javarouka@coupang.com>
2016-10-09 18:16:07 +0900

    add syntax highlighting

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
 import './css/index.css'
-import config from './config'
-console.log("%cNow Working...", "font-size: 200%; color: green;");
+import 'prismjs/themes/prism.css'
+import prism from 'prismjs'
+
+function applySyntaxHighliting() {
+    const syntaxTargets = document.querySelectorAll('pre code[data-language]');
+    syntaxTargets.forEach(target => {
+        const code = target.innerHTML;
+        var html = prism.highlight(code, prism.languages[target.dataset.language] || prism.languages.javascript);
+        target.innerHTML = html;
+    });
+}
+
+applySyntaxHighliting();
