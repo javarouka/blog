d7838dabe64f5d5d98d8ac64afa88d5a7a3bd9f1
이항희(JavaRouka) <javarouka@coupang.com>
2017-09-15 01:10:12 +0900

    remove prev theme

--- themes/hueman/source/js/external-link.js
+++
@@ -1,5 +0,0 @@
-(function ($) {
-    $('.article-entry a').filter(function(index, link) {
-        return link.hostname != window.location.hostname;
-    }).attr('target', '_blank').addClass('external-link');
-})(jQuery);
